59d2e3bfa1d514620deb7d9631d53d41b360fad9
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-13 14:27:51 +0530

    Update CoinFlipSimulator.java with comments and necessary info

--- src/coinFlipSimulator/CoinFlipSimulator.java
+++ src/coinFlipSimulator/CoinFlipSimulator.java
@@ -1,8 +1,20 @@
+/* Author Name: Laxmi Chari
+Roll No: 22
+Title: Program to implement a command line Game - Coin Flip Simulator
+Start Date: 25/08/2024
+Modified Date: 26/08/2024
+Description: This is the main class where ait creates a object of Game class and starts the game. 
+ */
+
 package coinFlipSimulator;
 
 public class CoinFlipSimulator {
+    // The main method.
     public static void main(String[] args) {
+        // Create an instance of the Game class.
         Game game = new Game();
+        
+        // Start the game, which allows the user to flip a coin repeatedly.
         game.start();
     }
 }
