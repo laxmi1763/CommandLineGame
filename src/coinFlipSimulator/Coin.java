97d4c55b88989211b70a3309990dbb8163157b03
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-13 14:27:17 +0530

    Update Coin.java with comments and necessary info

--- src/coinFlipSimulator/Coin.java
+++ src/coinFlipSimulator/Coin.java
@@ -1,13 +1,27 @@
+/* Author Name: Laxmi Chari
+Roll No: 22
+Title: Program to implement a command line Game - Coin Flip Simulator
+Start Date: 25/08/2024
+Modified Date: 26/08/2024
+Description: This program Generates a random value between 1 and 0 and gives heads and tails 
+ */
 package coinFlipSimulator;
 
 import java.util.Random;
 
 public class Coin {
+    // The current side of the coin facing up ("Heads" or "Tails").
     private String sideUp;
 
+    // The flip method simulates flipping the coin.
     public void flip() {
+        // Create a Random object to generate random numbers.
         Random random = new Random();
+        
+        // Generate a random number: 0 or 1.
         int result = random.nextInt(2); // Generates 0 or 1
+        
+        // If the result is 0, set sideUp to "Heads", otherwise set it to "Tails".
         if (result == 0) {
             sideUp = "Heads";
         } else {
@@ -15,6 +29,7 @@ public class Coin {
         }
     }
 
+    // This method returns the current side of the coin facing up.
     public String getSideUp() {
         return sideUp;
     }
